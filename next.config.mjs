ec16f9674aabc966120ad34fc26f6418eac88c5b
Petro Kvartsianyi <kvartsaniy@gmail.com>
2025-07-08 13:18:21 +0200

    fix: config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
-const repo = 'IfG-UAVs'; // GitHub repo name
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
+  output: 'export',
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
